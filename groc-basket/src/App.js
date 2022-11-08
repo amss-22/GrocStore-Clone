831826f120c00b25ab19c518de9e61c573ba7532
Amit Bhutani <107975368+amss-22@users.noreply.github.com>
2022-11-08 21:22:11 +0530

    added navbar

--- groc-basket/src/App.js
+++ groc-basket/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import NavBar from './orgasm/NavBar';
 import MainRoutes from './Routes/MainRoutes';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
      <MainRoutes/>
     </div>
   );
