68f8e23bb3175679300ea2e4eb515a5dea9a9b98
Amit Bhutani <107975368+amss-22@users.noreply.github.com>
2022-11-11 02:48:46 +0530

    added some changes

--- groc-basket/src/organisms/NavBar.jsx
+++ groc-basket/src/organisms/NavBar.jsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Searchbar from '../Components/Searchbar'
-import { getProductsData } from '../redux/NavigationReducer/action'
+import SearchFunc from "../Components/SearchFunc"
+
 
 const Navbar = () => {
-    const [query , setQuery]=useState("")
-    const dispatch=useDispatch()
-    const [suggestion ,setSuggestion]=useState([])
-    const products=useSelector((state)=> state.NavigationReducer.products)
-
-    const queryhandle=(data)=>{
-setQuery(data)
-    }
-    
-    useEffect(()=>{
-        dispatch(getProductsData())
-    },[])
-    
-        useEffect(()=>{
-              // dispatch(getProductsData()) 
-      //* filteration functionality will be done here 
-      
-      if(query === ""){
-            setSuggestion([])
-        }else{
-              let newSuggestion=products.filter((item)=>{
-                    return (item.product_info.toLowerCase().indexOf(query.toLowerCase())) !== -1  ? true : false 
-            
-                }).map((e)=> e.product_info)
-                setSuggestion(newSuggestion)
-            
-            }
-                },[query])
-            
+   
    
   return (
     <div>
-        <h1>Searchbar : {query}</h1>
-        <Searchbar queryhandle={queryhandle} suggestion={suggestion}/>
+        <SearchFunc/>
 
     </div>
   )
