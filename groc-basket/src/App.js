73d89b879d4aaf2fca4c79b01dc7c1c922c5225e
Manoj Rathod <myraysever@gmail.com>
2022-11-12 16:27:56 +0530

    added filter functionality

--- groc-basket/src/App.js
+++ groc-basket/src/App.js
@@ -4,7 +4,7 @@ import NavBar from './organisms/NavBar';
 import MainRoutes from './Routes/MainRoutes';
 import CartPage from './pages/CartPage';
 
-import HomePage  from './pages/HomePage';
+import HomePage from './pages/HomePage';
 
 
 import ProductPage from './pages/ProductPage';
@@ -15,22 +15,21 @@ function App() {
     <div >
 
       {/* <NavBar/> */}
-     {/* <MainRoutes/>
+      {/* <MainRoutes/>
      <CartPage /> */}
-     {/* <ProductPage/> */}
-    
+      <ProductPage />
 
-      <NavBar/>
-     {/* <MainRoutes/> */}
-    
+
+      <NavBar />
+      {/* <MainRoutes/> */}
+
 
       {/* <CartPage /> */}
-      {/* <HomePage/> */}
+      <HomePage />
 
     </div>
-    
+
   );
 }
 
 export default App;
-  
